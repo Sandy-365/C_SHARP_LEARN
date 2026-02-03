4614093549065a51a7cc3dd2d07522cdf7a89f69
Sandy-365 <sagarsandeep365@gmail.com>
2026-02-03 14:39:12 +0530

    SSMS files added

--- HelloWorldApp/obj/Debug/net10.0/HelloWorldApp.AssemblyInfo.cs
+++ HelloWorldApp/obj/Debug/net10.0/HelloWorldApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34085ef554eab169e23f01cce822e340c9b19101")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+339da29181e97baf785030d0ff761be18f4f4055")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
